779472d4736b916123680129d0b3f49527cdca0e
williammlekush <56037586+williammlekush@users.noreply.github.com>
2025-06-13 20:13:07 -0500

    fix: remove ORM dependency
    fix: shift from mysql2 to mysql

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 // server/server.js
 import express from "express";
 import mysql from "mysql2";
-import { Sequelize } from "sequelize";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -14,20 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 // Database configuration
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
 
 // Test the database connection
 async function testConnection() {
   try {
-    await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
