a4f3fc0d00b8f02802a922ebb86bc7cc8c6a3684
berosik <berosik@wisc.edu>
2025-06-29 20:05:30 -0500

    update vite.config.js to read PORT from server .env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
+export default defineConfig(({ mode}) => {
+  const env = loadEnv(mode, '../server/','');;
+  const PORT = parseInt(env.PORT) || 5000;
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://localhost:${PORT}`,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 });
