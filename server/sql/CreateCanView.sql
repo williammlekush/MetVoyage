926e6910be78feb3279377b7642b6952824dcfc1
berosik <berosik@wisc.edu>
2025-07-13 15:46:49 -0500

    switch can_view to use itinerary ID instead of object ID

--- server/sql/CreateCanView.sql
+++ server/sql/CreateCanView.sql
@@ -1,6 +1,6 @@
 # Can view relationship between Itineraries and Users
 CREATE TABLE IF NOT EXISTS Can_View (
    user_id INT REFERENCES Users(id)
-   ,object_id INT REFERENCES Objects(id)
-   ,PRIMARY KEY (user_id, object_id)
+   ,itinerary_id INT REFERENCES Itineraries(id)
+   ,PRIMARY KEY (user_id, itinerary_id)
 );
