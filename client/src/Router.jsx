3f8a7e2ed412ab6d875a24292cf69c002f460b9e
williammlekush <56037586+williammlekush@users.noreply.github.com>
2025-07-27 18:18:07 -0500

    feat: make authentication the landing page - closes #16

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,13 +1,13 @@
 // App.jsx or Router.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Art from "./modules/Art/components/Art";
-import App from "./App";
+import Auth from "./modules/Auth/components/Auth";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Auth />} />
         <Route path="/art" element={<Art userId={1} />} />
       </Routes>
     </BrowserRouter>
