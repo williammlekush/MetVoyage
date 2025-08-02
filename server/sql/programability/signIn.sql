ca328690744c0d952e1ed1ca66fa9cddeec231f1
williammlekush <56037586+williammlekush@users.noreply.github.com>
2025-08-02 16:24:51 -0500

    refactor: update sign in API to return th edisplay name and favorite object ID

--- server/sql/programability/signIn.sql
+++ server/sql/programability/signIn.sql
@@ -1,7 +1,12 @@
 CREATE PROCEDURE signIn(IN userName VARCHAR(255), IN passWord VARCHAR(255))
 BEGIN
-    SELECT id
+    SELECT 
+      id
+      ,display_name
+      ,favorite_obj_id
     FROM users
+    JOIN objects
+      ON users.favorite_obj_id = objects.id
     WHERE user_name = userName
     AND users.password = passWord;
 END;
